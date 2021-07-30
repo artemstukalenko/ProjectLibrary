8aaa9cfba3d235bb64114a9e714914eb5df42544
Artem Stukalenko <stukalenko.artem@gmail.com>
2021-07-30 11:05:20 +0300

    Added configure classes, still no deletions

---
+++ src/java/com/artemstukalenko/library/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.artemstukalenko.library.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
